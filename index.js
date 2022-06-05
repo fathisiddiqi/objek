f77b9e9caa1ca5f74e446964fd9fec4f0b3bb76e
fathisiddiqi <service.fathisiddiqi@gmail.com>
2022-06-05 17:03:18 +0700

    Modify index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 module.exports = {
   replaceObjectField: require("./functions/replaceObjectField"),
   valueToJSON: require("./functions/valueToJSON"),
+  stringify: require("./functions/stringify"),
+  objectify: require("./functions/objectify"),
 };
