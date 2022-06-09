8eca804b30799d1220f9f5e3fc26a7514855d280
fathisiddiqi <service.fathisiddiqi@gmail.com>
2022-06-09 08:16:29 +0700

    Modify test with recursive method

--- functions/clearEmptyValue.test.js
+++ functions/clearEmptyValue.test.js
@@ -1,6 +1,6 @@
 const clearEmptyValue = require("./clearEmptyValue");
 
-test("Clear empty value in an object", () => {
+test("Clear empty value in an object with recursive", () => {
   const exampleObj = {
     a: null,
     b: "",
@@ -20,11 +20,22 @@ test("Clear empty value in an object", () => {
           fca: "",
           fcb: {},
         },
+        {
+          fcc: "",
+          fcd: {},
+        },
       ],
       fd: 0,
       fe: undefined,
     },
   };
 
-  const objectCleared = clearEmptyValue({ object: exampleObj });
+  const objectCleared = clearEmptyValue({
+    object: exampleObj,
+    isRecursive: true,
+  });
+
+  expect(objectCleared).toEqual({
+    e: [{ b: "Test 2" }],
+  });
 });
