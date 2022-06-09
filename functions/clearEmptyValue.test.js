31875ff8217876b4d2d7879de5dea20c0ccedaa1
fathisiddiqi <service.fathisiddiqi@gmail.com>
2022-06-09 08:20:11 +0700

    Modify test with add ignoreNull

--- functions/clearEmptyValue.test.js
+++ functions/clearEmptyValue.test.js
@@ -1,41 +1,54 @@
 const clearEmptyValue = require("./clearEmptyValue");
 
-test("Clear empty value in an object with recursive", () => {
-  const exampleObj = {
-    a: null,
-    b: "",
-    c: {},
-    d: 0,
-    e: [
+const exampleObj = {
+  a: null,
+  b: "",
+  c: {},
+  d: 0,
+  e: [
+    {
+      a: "",
+      b: "Test 2",
+    },
+  ],
+  f: {
+    fa: "",
+    fb: {},
+    fc: [
       {
-        a: "",
-        b: "Test 2",
+        fca: "",
+        fcb: {},
+      },
+      {
+        fcc: "",
+        fcd: {},
       },
     ],
-    f: {
-      fa: "",
-      fb: {},
-      fc: [
-        {
-          fca: "",
-          fcb: {},
-        },
-        {
-          fcc: "",
-          fcd: {},
-        },
-      ],
-      fd: 0,
-      fe: undefined,
-    },
-  };
+    fd: 0,
+    fe: undefined,
+  },
+};
 
+test("Clear empty value in an object with recursive", () => {
   const objectCleared = clearEmptyValue({
     object: exampleObj,
     isRecursive: true,
   });
 
   expect(objectCleared).toEqual({
+    e: [{ b: "Test 2" }],
+  });
+});
+
+test('Clear empty value in an object with recursive and ignore "null"', () => {
+  const objectCleared = clearEmptyValue({
+    object: exampleObj,
+    isRecursive: true,
+    isIgnoreNull: true,
+  });
+
+  expect(objectCleared).toEqual({
+    a: null,
     e: [{ b: "Test 2" }],
   });
 });
