177bc49a46f752974a054ada7a1d0bed74622006
fathisiddiqi <service.fathisiddiqi@gmail.com>
2022-08-19 04:23:52 +0700

    Add example to objectify

--- functions/objectify.js
+++ functions/objectify.js
@@ -5,7 +5,7 @@
  * @since 1.0.0
  * @param {string} string - string to be parsed
  * @returns {string} string from object
- *
+ * @example objectify('{"name": "John"}') // {name: "John"}
  */
 module.exports = function (string) {
   return JSON.parse(string);
